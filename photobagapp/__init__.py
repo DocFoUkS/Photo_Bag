27bd01b208991c617350b37a953850549fbac249
Matveev Evgeniy <doc.fouks@yandex.ru>
2020-06-12 20:12:08 +0300

    Add boottrap, statis and new flask app

--- photobagapp/__init__.py
+++ photobagapp/__init__.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return u'Привет!'
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run()
